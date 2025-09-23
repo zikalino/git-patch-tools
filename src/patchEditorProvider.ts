c089ad28b75f20424064790ce88392801dde32ef
Zim Kalinowski <zkalinowski@opera.com>
2025-09-23 09:56:42 +0200

    moved html generation to single place

--- src/patchEditorProvider.ts
+++ src/patchEditorProvider.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
+import { getHtmlForWebview, getNonce } from './patchPanel';
+
 /**
  * Provider for cat scratch editors.
  * 
@@ -42,7 +44,7 @@ export class PatchEditorProvider implements vscode.CustomTextEditorProvider {
 		webviewPanel.webview.options = {
 			enableScripts: true,
 		};
-		webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview);
+		webviewPanel.webview.html = getHtmlForWebview(webviewPanel.webview, this.context.extensionUri, this.context);
 
 		function updateWebview() {
 			webviewPanel.webview.postMessage({
@@ -85,71 +87,4 @@ export class PatchEditorProvider implements vscode.CustomTextEditorProvider {
 
 		updateWebview();
 	}
-
-	/**
-	 * Get the static html used for the editor webviews.
-	 */
-	private getHtmlForWebview(webview: vscode.Webview): string {
-		// Local path to script and css for the webview
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'patchEditor.js'));
-
-		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'reset.css'));
-
-		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'vscode.css'));
-
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'patchEditor.css'));
-
-		// Use a nonce to whitelist which scripts can be run
-		const nonce = getNonce();
-
-		let htmlPath =  this.context.asAbsolutePath(path.join('media', 'patch-viewer.html'));
-		let html = fs.readFileSync(htmlPath, 'utf-8');
-
-
-		return html; 
-		
-		// /* html */`
-		// 	<!DOCTYPE html>
-		// 	<html lang="en">
-		// 	<head>
-		// 		<meta charset="UTF-8">
-
-		// 		<!--
-		// 		Use a content security policy to only allow loading images from https or from our extension directory,
-		// 		and only allow scripts that have a specific nonce.
-		// 		-->
-		// 		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-
-		// 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-		// 		<link href="${styleResetUri}" rel="stylesheet" />
-		// 		<link href="${styleVSCodeUri}" rel="stylesheet" />
-		// 		<link href="${styleMainUri}" rel="stylesheet" />
-
-		// 		<title>Cat Scratch</title>
-		// 	</head>
-		// 	<body>
-		// 		<div class="notes">
-		// 			<div class="add-button">
-		// 				<button>Scratch!</button>
-		// 			</div>
-		// 		</div>
-				
-		// 		<script nonce="${nonce}" src="${scriptUri}"></script>
-		// 	</body>
-		// 	</html>`;
-	}
-}
-
-function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
 }
--- src/patchPanel.ts
+++ src/patchPanel.ts
@@ -114,65 +114,66 @@ export class PatchPanel {
 
 	private _update() {
 		this._panel.title = "Patches";
-		this._panel.webview.html = this._getHtmlForWebview(this._panel.webview, "xxx");
+		this._panel.webview.html = getHtmlForWebview(this._panel.webview, this._extensionUri, this._context);
 	}
 
 
-	private _getHtmlForWebview(webview: vscode.Webview, catGifPath: string) {
-		// Local path to main script run in the webview
-		const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js');
-
-		// And the uri we use to load this script in the webview
-		const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
-
-		// Local path to css styles
-		const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css');
-		const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css');
-
-		// Uri to load styles into webview
-		const stylesResetUri = webview.asWebviewUri(styleResetPath);
-		const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
-
-		// Use a nonce to only allow specific scripts to be run
-		const nonce = getNonce();
-
-		let htmlPath =  this._context.asAbsolutePath(path.join('media', 'patch-viewer.html'));
-		let html = fs.readFileSync(htmlPath, 'utf-8');
-		
-		return html;
-		//`<!DOCTYPE html>
-		// 	<html lang="en">
-		// 	<head>
-		// 		<meta charset="UTF-8">
-
-		// 		<!--
-		// 			Use a content security policy to only allow loading images from https or from our extension directory,
-		// 			and only allow scripts that have a specific nonce.
-		// 		-->
-		// 		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
-
-		// 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-		// 		<link href="${stylesResetUri}" rel="stylesheet">
-		// 		<link href="${stylesMainUri}" rel="stylesheet">
-
-		// 		<title>Git Patch</title>
-		// 	</head>
-		// 	<body>
-		// 		<img src="${catGifPath}" width="300" />
-		// 		<h1 id="lines-of-code-counter">0</h1>
-
-		// 		<script nonce="${nonce}" src="${scriptUri}"></script>
-		// 	</body>
-		// 	</html>`;
-	}
 }
 
-function getNonce() {
+export function getNonce() {
 	let text = '';
 	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	for (let i = 0; i < 32; i++) {
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	}
 	return text;
+}
+
+export function getHtmlForWebview(webview: vscode.Webview, extensionUri: vscode.Uri, context: vscode.ExtensionContext) {
+	// Local path to main script run in the webview
+	const scriptPathOnDisk = vscode.Uri.joinPath(extensionUri, 'media', 'main.js');
+
+	// And the uri we use to load this script in the webview
+	const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+
+	// Local path to css styles
+	const styleResetPath = vscode.Uri.joinPath(extensionUri, 'media', 'reset.css');
+	const stylesPathMainPath = vscode.Uri.joinPath(extensionUri, 'media', 'vscode.css');
+
+	// Uri to load styles into webview
+	const stylesResetUri = webview.asWebviewUri(styleResetPath);
+	const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+
+	// Use a nonce to only allow specific scripts to be run
+	const nonce = getNonce();
+
+	let htmlPath =  context.asAbsolutePath(path.join('media', 'patch-viewer.html'));
+	let html = fs.readFileSync(htmlPath, 'utf-8');
+	
+	return html;
+	//`<!DOCTYPE html>
+	// 	<html lang="en">
+	// 	<head>
+	// 		<meta charset="UTF-8">
+
+	// 		<!--
+	// 			Use a content security policy to only allow loading images from https or from our extension directory,
+	// 			and only allow scripts that have a specific nonce.
+	// 		-->
+	// 		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+
+	// 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	// 		<link href="${stylesResetUri}" rel="stylesheet">
+	// 		<link href="${stylesMainUri}" rel="stylesheet">
+
+	// 		<title>Git Patch</title>
+	// 	</head>
+	// 	<body>
+	// 		<img src="${catGifPath}" width="300" />
+	// 		<h1 id="lines-of-code-counter">0</h1>
+
+	// 		<script nonce="${nonce}" src="${scriptUri}"></script>
+	// 	</body>
+	// 	</html>`;
 }
